2cf77bb2bd7b3a2f42d9224e43a3c772ff9a1538
Stefan Holmberg <stefan.holmberg@systementor.se>
2023-08-12 12:39:23 +0200

    Smhi

--- src/main/java/se/systementor/webservicejavadag1/services/PredictionService.java
+++ src/main/java/se/systementor/webservicejavadag1/services/PredictionService.java
@@ -2,6 +2,7 @@ package se.systementor.webservicejavadag1.services;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import se.systementor.webservicejavadag1.models.WeatherPrediction;
 
@@ -9,6 +10,7 @@ import java.io.IOException;
 import java.io.StringWriter;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -62,15 +64,15 @@ public class PredictionService {
 
 
     private List<WeatherPrediction> readAllFromFile() throws IOException {
-        if(!Files.exists(Path.of("predictions.json"))) return new ArrayList<WeatherPrediction>();
-        ObjectMapper objectMapper = getObjectMapper();
-        var jsonStr = Files.readString(Path.of("predictions.json"));
-        return  new ArrayList(Arrays.asList(objectMapper.readValue(jsonStr, WeatherPrediction[].class ) ));
+        if(!Files.exists(Path.of("predictions.xml"))) return new ArrayList<WeatherPrediction>();
+        var objectMapper = getObjectMapper();
+        var xmlStr = Files.readString(Path.of("predictions.xml"));
+        return  new ArrayList(Arrays.asList(objectMapper.readValue(xmlStr, WeatherPrediction[].class ) ));
     }
 
 
     private void writeAllToFile(List<WeatherPrediction> weatherPredictions) throws IOException {
-        ObjectMapper objectMapper = getObjectMapper();
+        var objectMapper = getObjectMapper();
         objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
         objectMapper.disable(SerializationFeature.FAIL_ON_EMPTY_BEANS);
 
@@ -78,14 +80,15 @@ public class PredictionService {
         StringWriter stringWriter = new StringWriter();
         objectMapper.writeValue(stringWriter, weatherPredictions);
 
-        Files.writeString(Path.of("predictions.json"), stringWriter.toString());
+        Files.writeString(Path.of("predictions.xml"), stringWriter.toString());
 
     }
 
 
-    private static ObjectMapper getObjectMapper() {
-        ObjectMapper mapper = new ObjectMapper();
+    private static XmlMapper getObjectMapper() {
+        var mapper = new XmlMapper();
         mapper.registerModule(new JavaTimeModule());
+        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
         return mapper;
     }
 
